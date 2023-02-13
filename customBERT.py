cf491d37aa3d51871ff10e0d08b59961d8c6f925
Parhamglst <parhamglst@gmail.com>
2023-02-13 00:15:43 -0700

    Bugs fixed

--- customBERT.py
+++ customBERT.py
@@ -1,56 +1,55 @@
 import torch
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader, random_split
-from transformers import AutoTokenizer, Trainer, TrainingArguments
+from transformers import AutoTokenizer, AdamW
 from transformers.models import bert
 from datasets import DatasetDict
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 
+def read_dataset(file_path:str, tokenizer:AutoTokenizer):
+    dataset = pd.read_csv(file_path)
+    df = dataset.iloc[1:50000,:]
+    for i in range(len(df.values)):
+        df.values[i, 1] =  1 if df.values[i, 1] == 'positive' else 0
+    x_encodings = tokenizer(list(df.values[:,0]), truncation=True, padding ='max_length')
+    data = {
+        "input_ids": x_encodings.input_ids,
+        "attention_mask":x_encodings.attention_mask,
+        "labels":df.values[:,1]
+            }
+    return data
+
 class CustomDataset(Dataset):
-    def __init__(self, fp):
-        self.fp = fp
-        self.data = pd.read_csv(self.fp)
-        self.tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-        
-        self.x = self.data.iloc[1:701, 0]
-        self.y = self.data.iloc[1:701, 1]
-        
-        # Want integer values instead of 'positive'/'negative'
+    def __init__(self, data):
+        self.data = data
     
     def __len__(self):
-        return len(self.y)
+        return len(self.data['input_ids'])
     
     def __getitem__(self, index):
-        inputs = self.tokenizer(self.x[index], truncation=True, padding='max_length')
-        # inputs = torch.tensor(inputs.encodings)
-        # I can't convert input into tensor
-        
-        # labels = self.tokenizer(self.y[index], truncation=True, padding=True)
-        labels = 1 if self.y[index] == 'positive' else 0
-        inputs['labels'] = torch.tensor(labels)
-        return inputs
-
-dataset = CustomDataset('IMDB_Dataset.csv')
-
-train_set, eval_set = random_split(dataset, [600, 100])
-
-training_args = TrainingArguments(output_dir='./results',
-                                  num_train_epochs=3,
-                                  per_device_train_batch_size=16,
-                                  per_device_eval_batch_size=10,
-                                  warmup_steps=500,
-                                  weight_decay=0.01,
-                                  logging_dir='./logs',
-                                  logging_steps=10)
+        return {key: torch.tensor(val[index]) for key, val in self.data.items()}
+    
+torch.cuda.empty_cache()
+data = read_dataset('IMDB_Dataset.csv', AutoTokenizer.from_pretrained("bert-base-cased"))
+dataset = CustomDataset(data)
+dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
 
 model = bert.BertForSequenceClassification.from_pretrained("bert-base-cased", num_labels=2)
+model.to(device)
+model.train()
 
-trainer = Trainer(model=model,
-                  args=training_args,
-                  train_dataset=train_set,
-                  eval_dataset = eval_set)
+optimizer = AdamW(model.parameters())
+for epoch in range(5):
+    for batch in dataloader:
+        optimizer.zero_grad()
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs[0]
+        loss.backward()
+        optimizer.step()
 
-trainer.train()
-    
+model.eval()
